27a693aead04a68bd4fb789d0db8196ffa6c48fb
kamil-gorny <kamil.gorny99@gmail.com>
2022-03-22 00:29:50 +0100

    feat: Add local json-server

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -9,6 +9,10 @@ export default {
   name: "Post",
   components:{
     PostHeader,
+  },
+  async beforeRouteEnter(to, from, next){
+    const res = await fetch(`http://localhost:5003/posts/${to.params.id}`);
+    const post = await res.json();
   }
 }
 </script>
