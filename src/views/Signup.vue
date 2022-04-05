23228a7d38e1c93fc0d40008968109e4f270e23f
kamil-gorny <kamil.gorny99@gmail.com>
2022-04-05 23:57:20 +0200

    feat: Login automatically after signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,37 +1,38 @@
 <template>
   <form class="register-form" @submit="onSubmit">
-      <div class="form-header">
+    <div class="form-header">
       <h1>Sign up</h1>
-      <p>Create  account to start sharing your thoughts</p>
-      </div>
-      <hr>
+      <p>Create account to start sharing your thoughts</p>
+    </div>
+    <hr>
 
-      <label for="firstName"><b>First Name</b></label>
-      <input type="text" placeholder="John" name="firstName" id="firstName" v-model="user.FirstName" required>
+    <label for="firstName"><b>First Name</b></label>
+    <input type="text" placeholder="John" name="firstName" id="firstName" v-model="user.FirstName" required>
 
-      <label for="lastName"><b>Last Name</b></label>
-      <input type="text" placeholder="Smith" name="lastName" id="lastName" v-model="user.LastName" required>
+    <label for="lastName"><b>Last Name</b></label>
+    <input type="text" placeholder="Smith" name="lastName" id="lastName" v-model="user.LastName" required>
 
-      <label for="userName"><b>Username</b></label>
-      <input type="text" placeholder="john-smith" name="userName" id="userName" v-model="user.UserName" required>
+    <label for="userName"><b>Username</b></label>
+    <input type="text" placeholder="john-smith" name="userName" id="userName" v-model="user.UserName" required>
 
-      <label for="email"><b>Email</b></label>
-      <input type="email" placeholder="john-smith@example.com" name="email" id="email" v-model="user.Email" required>
+    <label for="email"><b>Email</b></label>
+    <input type="email" placeholder="john-smith@example.com" name="email" id="email" v-model="user.Email" required>
 
-      <label for="password"><b>Password</b></label>
-      <input type="password" placeholder="**********" name="password" id="password" v-model="user.Password" required>
-      <hr>
-      <button type="submit" class="registerbtn">Sign up</button>
+    <label for="password"><b>Password</b></label>
+    <input type="password" placeholder="**********" name="password" id="password" v-model="user.Password" required>
+    <hr>
+    <button type="submit" class="registerbtn">Sign up</button>
   </form>
 </template>
 
 <script>
 import axios from "axios";
+import router from "@/router";
 
 export default {
   name: "Register",
-  data(){
-    return{
+  data() {
+    return {
       user: {
         FirstName: '',
         LastName: '',
@@ -41,38 +42,54 @@ export default {
       },
     }
   },
-  methods:{
-    async onSubmit(e){
+  methods: {
+    async onSubmit(e) {
       e.preventDefault()
-       await axios.post("http://localhost:5222/api/auth/register",this.user,{
-         headers: {
-           'Content-Type': 'application/json',
-         },
-       });
+      await axios.post("http://localhost:5222/api/auth/register", this.user, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(async () => {
+        await axios.post("http://localhost:5222/api/auth/login", {
+          Email: this.user.Email,
+          Password: this.user.Password
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }).then(res => {
+          localStorage.setItem("token", res.data);
+          router.push({path: '/'})
+        }, err => console.log(err));
+      });
     }
   }
 }
 </script>
 
 <style scoped>
-.form-header{
+.form-header {
   display: flex;
   flex-direction: column;
   justify-content: center;
   text-align: center;
 }
-h1{
+
+h1 {
   font-size: 40px;
   font-weight: bold;
 }
-label{
+
+label {
   font-size: 18px;
 }
-p{
+
+p {
   font-size: 18px;
   color: #D6D8DA;
 }
-.register-form{
+
+.register-form {
   display: flex;
   flex-direction: column;
   width: 40%;
@@ -80,7 +97,8 @@ p{
   margin-left: auto;
   margin-right: auto;
 }
-input{
+
+input {
   height: 40px;
   margin-top: 10px;
   margin-bottom: 20px;
@@ -90,15 +108,17 @@ input{
   font-size: 18px;
   line-height: 18px;
 }
-input::placeholder{
+
+input::placeholder {
   font-weight: bold;
-  color:#D6D8DA;
+  color: #D6D8DA;
   line-height: 18px;
 
 }
-button{
+
+button {
   height: 40px;
-  border:none;
+  border: none;
   background-color: #0583D2;
   font-weight: bold;
   color: #fff;
